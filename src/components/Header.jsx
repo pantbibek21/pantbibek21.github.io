e63fa9586d6586f26489c472cf4e125a08870cff
Bibek Pant <pantbibek21@gmail.com>
2025-09-22 10:56:43 +0200

    style: add responsiveness on header and improvise hero style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
 import styles from "./Header.module.scss";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { CgClose } from "react-icons/cg";
+import { useState } from "react";
 
 const Header = () => {
+  const [mobileMenuActive, isMobileMenuActive] = useState(false);
+
   return (
     <header className={styles.header}>
       <nav>
-        <ul>
+        <ul className={`${mobileMenuActive ? styles.active : ""}`}>
           <li>
             <a href="#">about</a>
           </li>
@@ -16,6 +21,22 @@ const Header = () => {
           </li>
         </ul>
       </nav>
+      <div
+        className={` ${styles.mobileMenu} ${
+          !mobileMenuActive ? styles.menuActive : ""
+        }`}
+        onClick={() => isMobileMenuActive(!mobileMenuActive)}
+      >
+        <RxHamburgerMenu />
+      </div>
+      <div
+        className={` ${styles.mobileClose} ${
+          mobileMenuActive ? styles.menuCloseActive : ""
+        }`}
+        onClick={() => isMobileMenuActive(!mobileMenuActive)}
+      >
+        <CgClose />
+      </div>
     </header>
   );
 };
